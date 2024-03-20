ca96230f9dd8b5beeca732cca5f43cb8f2b2d201
BellWin98 <jongseunghan@naver.com>
2024-03-20 16:47:06 +0900

    [Feat] 회원 Entity & S3 일부 기능 구현(#7)

---
+++ src/main/java/com/team1/moim/domain/member/entity/Role.java
@@ -0,0 +1,8 @@
+package com.team1.moim.domain.member.entity;
+
+import lombok.Getter;
+
+@Getter
+public enum Role {
+    ROLE_USER, ROLE_ADMIN
+}
--- src/main/java/com/team1/moim/domain/member/repository/MemberRepository.java
+++ src/main/java/com/team1/moim/domain/member/repository/MemberRepository.java
@@ -4,6 +4,10 @@ import com.team1.moim.domain.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findByNickname(String nickname);
+    Optional<Member> findByEmail(String email);
 }
