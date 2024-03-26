87a291e354f5022bbd81a377eb49d10c3559e24b
BellWin98 <jongseunghan@naver.com>
2024-03-26 12:38:25 +0900

    [Chore] SecurityConfig 설정(#7)
    * Java 17 버전에 맞게 구성

--- src/main/java/com/team1/moim/global/config/security/SecurityConfig.java
+++ src/main/java/com/team1/moim/global/config/security/SecurityConfig.java
@@ -1,16 +1,59 @@
 package com.team1.moim.global.config.security;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
+@Configuration
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true) // 메소드 호출 이전 이후에 권한을 확인할 수 있다.
+@EnableMethodSecurity
 public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder(){
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception{
+        return httpSecurity
+                // 쿠키와 세션을 사용하는 MVC패턴에서 csrf 공격에 취약하나, REST API는 덜 취약하여 비활성화
+                .csrf(AbstractHttpConfigurer::disable)
+
+                // CORS 활성화(특정 도메인만 접속할 수 있도록 허용)
+                .cors(AbstractHttpConfigurer::disable)
+                .formLogin(AbstractHttpConfigurer::disable)
+
+                // 기본 인증 로그인 사용 안하므로 disable
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authorizeRequest ->
+                        authorizeRequest
+                                .requestMatchers("/" ).permitAll()
+                                .requestMatchers(EVENT_API_URL).permitAll()
+                                .requestMatchers(AUTH_API_URL).permitAll()
+                                .anyRequest()
+                                .authenticated()
+                )
+                // 세션을 사용하지 않는 설정 추가
+                .sessionManagement((sessionManagement) ->
+                        sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                .build();
+    }
+
+    private static final String[] EVENT_API_URL = {
+            "/api/events",
+            "/api/events/**"
+    };
+
+    private static final String[] AUTH_API_URL = {
+            "/api/auth",
+            "/api/auth/**"
+    };
 }
