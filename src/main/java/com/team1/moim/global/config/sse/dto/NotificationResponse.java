9e5b6f999a1a9b85d820c4fa3e2324ef49a50446
qoth-0 <bny1324@naver.com>
2024-04-09 12:04:55 +0900

    [Feat] readYn 추가

--- src/main/java/com/team1/moim/global/config/sse/dto/NotificationResponse.java
+++ src/main/java/com/team1/moim/global/config/sse/dto/NotificationResponse.java
@@ -4,6 +4,8 @@ import com.team1.moim.domain.event.entity.Alarm;
 import com.team1.moim.domain.member.entity.Member;
 import java.time.LocalDateTime;
 
+import com.team1.moim.domain.member.entity.Role;
+import com.team1.moim.domain.member.entity.SocialType;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -13,20 +15,28 @@ import org.springframework.data.redis.core.RedisHash;
 import org.springframework.data.redis.core.TimeToLive;
 
 @NoArgsConstructor
-@AllArgsConstructor
 @Getter
-@Builder
 public class NotificationResponse {
     @Id
-    private Long id;
+    private Long alarmId;
     private String nickname;
     private String message;
     private String sendTime;
+    private String readYn = "N";
+
+    @Builder
+    public NotificationResponse(Long alarmId, String nickname, String message, String sendTime) {
+        this.alarmId = alarmId;
+        this.nickname = nickname;
+        this.message = message;
+        this.sendTime = sendTime;
+    }
+
 
     public static NotificationResponse from(Alarm alarm, Member member, LocalDateTime sendTime){
         String message = alarm.getSetTime() + alarm.getAlarmtype().toString()+"전 알람입니다.";
         return NotificationResponse.builder()
-                .id(alarm.getId())
+                .alarmId(alarm.getId())
                 .nickname(member.getNickname())
                 .message(message)
                 .sendTime(sendTime.toString())
