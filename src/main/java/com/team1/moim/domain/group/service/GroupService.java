ae3174c0560754d2dca4a5390afd82e3f81d2190
JangEunJi <>
2024-04-24 20:38:15 +0900

    모임 response 수정

--- src/main/java/com/team1/moim/domain/group/service/GroupService.java
+++ src/main/java/com/team1/moim/domain/group/service/GroupService.java
@@ -219,8 +219,6 @@ public class GroupService {
 
         List<ListGroupResponse> groupResponse = new ArrayList<>();
 
-
-
         for(Group group: groups){ // 자기가 속한 모든 그룹의 정보를 추출
             List<String[]> guestEmailNicknameIsAgreed = new ArrayList<>(); // 각 그룹의 게스트 이메일, 닉네임, 동의여부
             List<GroupInfo> tempGroupInfos = groupInfoRepository.findByGroup(group);
@@ -228,57 +226,13 @@ public class GroupService {
             for(GroupInfo groupInfo : tempGroupInfos){
                 guestEmailNicknameIsAgreed.add(new String[]{groupInfo.getMember().getEmail(),
                                                             groupInfo.getMember().getNickname(),
-                                                            groupInfo.getIsAgreed()});
+                                                            groupInfo.getIsAgreed(),
+                                                            String.valueOf(groupInfo.getId())});
 
             }
             groupResponse.add(ListGroupResponse.from(group, guestEmailNicknameIsAgreed));
         }
 
-
-//        for(Group group1: groups){
-//            groupResponse.add(ListGroupResponse.from(group1, guestEmailNicknameIsAgreed));
-//        }
-
-
-
-//        Specification<Group> spec = new Specification<>() {
-//            @Override
-//            public Predicate toPredicate(Root<Group> root, CriteriaQuery<?> query, CriteriaBuilder criteriaBuilder) {
-//                List<Predicate> predicates = new ArrayList<>();
-//
-//                // Group 테이블과 GroupInfo 테이블을 조인하고, 그 결과에서 특정 멤버의 이메일이 loginEmail과 같은 그룹만 선택
-//                // SELECT * FROM Group g
-//                // JOIN GroupInfo gi ON g.id = gi.group_id
-//                // WHERE gi.member.email = :loginEmail
-//                Join<Group, GroupInfo> groupJoin = root.join("groupInfos");
-//                predicates.add(criteriaBuilder.equal(groupJoin.get("member").get("email"), loginEmail));
-//
-//                // 삭제되지 않은 그룹
-//                predicates.add(criteriaBuilder.equal(root.get("isDeleted"), "N"));
-//
-//                // filterConfirmed가 true일 경우, 명시적으로 "확정"(isConfirmed가 "Y")된 그룹을 검색
-//                // filterWaiting가 true일 경우, "대기" 상태(isConfirmed가 "N"이고 isAgreed가 "N")인 그룹을 검색
-//                // filterAdjusting가 true일 경우, "조율" 상태(isConfirmed가 "N"이고 isAgreed가 "Y")인 그룹을 검색
-//                // 사용자가 특정 상태의 그룹만 보고 싶다면, 해당 상태에 대한 필터를 true로 설정.
-//                // 모든 그룹을 확인하고 싶다면, 모든 필터를 false로 두거나 설정하지 않으면 됨.
-//
-//                if (groupSearchRequest.isFilterConfirmed()) {
-//                    predicates.add(criteriaBuilder.equal(groupJoin.get("isConfirmed"), "Y"));
-//                } else if (groupSearchRequest.isFilterWaiting()) {
-//                    predicates.add(criteriaBuilder.equal(groupJoin.get("isConfirmed"), "N"));
-//                    predicates.add(criteriaBuilder.equal(groupJoin.get("isAgreed"), "N"));
-//                } else if (groupSearchRequest.isFilterAdjusting()) {
-//                    predicates.add(criteriaBuilder.equal(groupJoin.get("isConfirmed"), "N"));
-//                    predicates.add(criteriaBuilder.equal(groupJoin.get("isAgreed"), "Y"));
-//                }
-//
-//                return criteriaBuilder.and(predicates.toArray(new Predicate[0]));
-//            }
-//        };
-
-//        List<ListGroupResponse> groups = groupRepository.findAll( pageable).stream()
-//                .map(ListGroupResponse::from)
-//                .collect(Collectors.toList());
         return groupResponse;
     }
 
